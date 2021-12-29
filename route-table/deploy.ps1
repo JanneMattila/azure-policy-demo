a3400f568dac10cfb36d195dbef2efec03955dee
Janne Mattila <janne.mattila@hotmail.fi>
2021-12-29 17:55:19 +0200

    Update example

--- route-table/deploy.ps1
+++ route-table/deploy.ps1
@@ -17,7 +17,7 @@ $job
 $job | Wait-Job
 
 # Policy definition name
-$auditRouteTables = "audit-route-tables"
+$auditRouteTables = "RoutingTable Audit"
 
 # Create policy for verifying that route tables contain mandatory routes
 $auditRouteTablesDefinition = New-AzPolicyDefinition `
@@ -35,26 +35,43 @@ $auditRouteTablesAssignment = New-AzPolicyAssignment `
 ################
 # Create routes
 ################
-# - Correct route
+# Correct mandatory routes
 $route1 = New-AzRouteConfig -Name "Route01" -AddressPrefix "0.0.0.0/0" -NextHopType "VirtualAppliance" -NextHopIpAddress "10.20.30.40"
-# - Incorrect route
-$route1 = New-AzRouteConfig -Name "Route01" -AddressPrefix "0.0.0.0/0" -NextHopType "VirtualAppliance" -NextHopIpAddress "11.21.31.41"
-
-# Required routes
 $route2 = New-AzRouteConfig -Name "Route02" -AddressPrefix "1.2.0.0/21" -NextHopType "VirtualAppliance" -NextHopIpAddress "10.20.30.40"
 $route3 = New-AzRouteConfig -Name "Route03" -AddressPrefix "11.22.33.0/25" -NextHopType "VirtualAppliance" -NextHopIpAddress "10.20.30.40"
+
+# Incorrect route (but similar to route01)
+$route1incorrect = New-AzRouteConfig -Name "Route01wrong" -AddressPrefix "0.0.0.0/0" -NextHopType "VirtualAppliance" -NextHopIpAddress "11.21.31.41"
 
 # Additional route
 $route4 = New-AzRouteConfig -Name "Route04" -AddressPrefix "100.200.230.0/25" -NextHopType "VirtualAppliance" -NextHopIpAddress "1.2.3.4"
 
-# Notice that name contains "important"
-$routeName1 = "App-important-rt"
+# Notice that name contains "important-rt"
+$routeTableValid = "App-Valid-important-rt"
 
 # Create route table
-$routeTable = New-AzRouteTable `
-    -Name $routeName1 `
+New-AzRouteTable `
+    -Name $routeTableValid `
     -ResourceGroupName $resourceGroupName `
     -Location $location -Route $route1, $route2, $route3, $route4
+
+# Notice that name does not contain "important" and will be ignored
+$routeTableIgnored = "App-Ignored-rt"
+
+# Create route table without routes
+New-AzRouteTable `
+    -Name $routeTableIgnored `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location -Route $route1, $route2, $route3, $route4
+
+# Notice that name does contain "important"
+$routeTableInvalid = "App-Invalid-important-rt"
+
+# Create route table without routes
+New-AzRouteTable `
+    -Name $routeTableInvalid `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location -Route $route1incorrect, $route2, $route3, $route4
 
 ###################################
 #   ____ _     _____    _    _   _
@@ -65,8 +82,10 @@ $routeTable = New-AzRouteTable `
 # your resources
 ###################################
 
-# Remove route table
-Remove-AzRouteTable -Name $routeName1 -ResourceGroupName $resourceGroupName -Force
+# Remove route tables
+Remove-AzRouteTable -Name $routeTableValid -ResourceGroupName $resourceGroupName -Force
+Remove-AzRouteTable -Name $routeTableIgnored -ResourceGroupName $resourceGroupName -Force
+Remove-AzRouteTable -Name $routeTableInvalid -ResourceGroupName $resourceGroupName -Force
 
 # Wipe out the policy resources
 Remove-AzPolicyAssignment -Name $auditRouteTables -Scope $resourceGroup.ResourceId
